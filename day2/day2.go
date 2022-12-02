4fe32b5e02bd8e28db700c1ce68137985d23f788
Fredrik Lack <fredrik@sthlm.io>
2022-12-02 17:52:19 +0100

    Completing Day 2 (part 2)

--- day2/day2.go
+++ day2/day2.go
@@ -7,9 +7,9 @@ import (
 )
 
 const (
-	RockScore    = 1
-	PaperScore   = 2
-	ScissorScore = 3
+	Rock    = 1
+	Paper   = 2
+	Scissor = 3
 )
 
 const (
@@ -33,26 +33,64 @@ func gameRounds() []string {
 func play(game string) (int, int) {
 	switch game {
 	case "AX":
-		return RockScore + DrawScore, RockScore + DrawScore
+		return Rock, Rock
 	case "BY":
-		return PaperScore + DrawScore, PaperScore + DrawScore
+		return Paper, Paper
 	case "CZ":
-		return ScissorScore + DrawScore, ScissorScore + DrawScore
+		return Scissor, Scissor
 	case "AY":
-		return RockScore, PaperScore + WinScore
+		return Rock, Paper
 	case "BX":
-		return PaperScore + WinScore, RockScore
+		return Paper, Rock
 	case "AZ":
-		return RockScore + WinScore, ScissorScore
+		return Rock, Scissor
 	case "CX":
-		return ScissorScore, RockScore + WinScore
+		return Scissor, Rock
 	case "BZ":
-		return PaperScore, ScissorScore + WinScore
+		return Paper, Scissor
 	case "CY":
-		return ScissorScore + WinScore, PaperScore
+		return Scissor, Paper
 	}
 
 	return 0, 0
+}
+
+func playFixed(game string) (int, int) {
+	switch game {
+	case "AX":
+		return Rock, Scissor
+	case "BY":
+		return Paper, Paper
+	case "CZ":
+		return Scissor, Rock
+	case "AY":
+		return Rock, Rock
+	case "BX":
+		return Paper, Rock
+	case "AZ":
+		return Rock, Paper
+	case "CX":
+		return Scissor, Paper
+	case "BZ":
+		return Paper, Scissor
+	case "CY":
+		return Scissor, Scissor
+	}
+
+	return 0, 0
+}
+
+func calcWinner(s1 int, s2 int) (int, int) {
+	if s1 == Rock && s2 == Scissor || s1 == Paper && s2 == Rock || s1 == Scissor && s2 == Paper {
+		s1 += WinScore
+	} else if s1 == s2 {
+		s1 += DrawScore
+		s2 += DrawScore
+	} else {
+		s2 += WinScore
+	}
+
+	return s1, s2
 }
 
 func Part1() (int, int) {
@@ -61,7 +99,7 @@ func Part1() (int, int) {
 
 	games := gameRounds()
 	for _, game := range games {
-		p1, p2 := play(game)
+		p1, p2 := calcWinner(play(game))
 		opponentScore += p1
 		myScore += p2
 	}
@@ -69,6 +107,16 @@ func Part1() (int, int) {
 	return opponentScore, myScore
 }
 
-func Part2() int {
-	panic("not implemented yet")
+func Part2() (int, int) {
+	opponentScore := 0
+	myScore := 0
+
+	games := gameRounds()
+	for _, game := range games {
+		p1, p2 := calcWinner(playFixed(game))
+		opponentScore += p1
+		myScore += p2
+	}
+
+	return opponentScore, myScore
 }
--- day2/day2_test.go
+++ day2/day2_test.go
@@ -14,3 +14,14 @@ func TestPart1(t *testing.T) {
 		t.Fatalf(`Want: %v (opponentScore), %v (myScore) --> return value: %v, %v`, opponentScoreWant, myScoreWant, opponentScore, myScore)
 	}
 }
+
+func TestPart2(t *testing.T) {
+	opponentScore, myScore := Part2()
+
+	opponentScoreWant := 11921
+	myScoreWant := 13071
+
+	if myScore != opponentScoreWant && myScore != myScoreWant {
+		t.Fatalf(`Want: %v (opponentScore), %v (myScore) --> return value: %v, %v`, opponentScoreWant, myScoreWant, opponentScore, myScore)
+	}
+}
